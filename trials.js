5ba4bf92ed0b06f4845b563ba0d6dbadcfac62d1
Ray Shani <raysainepushani@gmail.com>
2025-04-29 13:49:38 +0300

    First commit:string concatenation practices

---
+++ trials.js
@@ -0,0 +1,24 @@
+
+// Restarting of javascript practicals
+// Concatenation
+let myName="Ray";
+let herName="Sashi";
+console.log(myName+ "" + "and" +""+ herName + "are brother and sister");
+myName.length
+
+let car="This is a subaru";
+let owner="John Njoroge";
+console.log(car + "and it is owned by a friend of mine" + owner);
+
+let daily3="Movement,meditation,morning pages";
+let movement="When i wake up everyday i will ensure I practise :";
+console.log(movement + daily3);
+
+let lawsOfNature="The laws of the jungle are very intense for a young man";
+let lawsOfEmployment="A major one in the business world is hire slowly and fire quickly";
+console.log("General thoughts dictate that" + "" + lawsOfNature + "" + lawsOfEmployment);
+
+let excellence="To seek excellence you must first taste the bitter tatse of failure";
+let growth="Cumulative growth as person is the same as deep as his conviction of not failing";
+console.log(excellence.length);
+console.log("What is life with excellence,"+ "" + excellence + "" +growth);
